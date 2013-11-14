2dc3d439eccae34a479ff290db7280fdf4b7df3c
Haegeun Park <haegeun.park@samsung.com>
2013-11-13 20:46:01 -0800

    [INTERNAL] Added gl3 headers in include

---
+++ include/GLES3/gl3.h
@@ -0,0 +1,14 @@
+#ifndef __gl3_h_
+#define __gl3_h_
+
+#define _COREGL_NAME_MANGLE(name) name
+
+#include <GLES3/gl3platform.h>
+
+#include "def_gl.h"
+#include "sym_gl.h"
+
+#undef GL
+
+#endif /* __gl3_h_ */
+
